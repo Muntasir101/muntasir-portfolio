06d588231e64429cbda4dae8006f9c34d81430f0
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-11-19 10:14:10 +0600

    update intro

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,6 +6,7 @@ function Type() {
     <Typewriter
       options={{
         strings: [
+          "Software Tester",
           "Software Test Automation Engineer",
           "SDET",
           "Open Source Contributor",
